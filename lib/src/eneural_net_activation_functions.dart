c2f569d1461ef5afe2bd6044154d61ec23f5a60e
gmpassos <gracilianomp@gmail.com>
2021-05-26 04:15:11 -0300

    - `ActivationFunction`:   - Added field `flatSpot` for `derivativeEntryWithFlatSpot()`.   - Added `ActivationFunctionLinear`.   - `ActivationFunctionSigmoid`: activation with bounds (-700 .. 700).

--- lib/src/eneural_net_activation_functions.dart
+++ lib/src/eneural_net_activation_functions.dart
@@ -1,11 +1,31 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'eneural_net_fastmath.dart' as fast_math;
 
-abstract class ActivationFunction<N extends num, E> {
-  final int initialWeightScale;
+enum ActivationFunctionScope { input, hidden, output, any }
 
-  const ActivationFunction(this.initialWeightScale);
+abstract class ActivationFunction<N extends num, E> {
+  static const double TOO_SMALL = -1.0E20;
+  static const double TOO_BIG = 1.0E20;
+
+  final double initialWeightScale;
+  final double initialWeightBiasValue;
+  final double flatSpot;
+
+  const ActivationFunction(this.initialWeightScale,
+      {this.initialWeightBiasValue = 0.0, this.flatSpot = 0.0001});
+
+  List<ActivationFunctionScope> get scope;
+
+  double createRandomWeight(Random random, {double? scale}) {
+    scale ??= initialWeightScale;
+    return (random.nextDouble() * (scale * 2)) - scale;
+  }
+
+  List<double> createRandomWeights(Random random, int length, {double? scale}) {
+    return List.generate(length, (index) => createRandomWeight(random));
+  }
 
   N activate(N x);
 
@@ -13,34 +33,110 @@ abstract class ActivationFunction<N extends num, E> {
 
   N derivative(N o);
 
+  N derivativeWithFlatSpot(N o) {
+    return derivative(o);
+  }
+
   E derivativeEntry(E entry);
+
+  E derivativeEntryWithFlatSpot(E entry) {
+    return derivativeEntry(entry);
+  }
 
   @override
   String toString() => runtimeType.toString();
 }
 
-class ActivationFunctionSigmoid extends ActivationFunction<double, Float32x4> {
-  const ActivationFunctionSigmoid([int initialWeightScale = 2])
+class ActivationFunctionLinear extends ActivationFunction<double, Float32x4> {
+  const ActivationFunctionLinear({double initialWeightScale = 1})
       : super(initialWeightScale);
+
+  static final List<ActivationFunctionScope> _scope =
+      List.unmodifiable([ActivationFunctionScope.input]);
+
+  @override
+  List<ActivationFunctionScope> get scope => _scope;
 
   @override
   double activate(double x) {
+    return x;
+  }
+
+  @override
+  Float32x4 activateEntry(Float32x4 entry) {
+    return entry;
+  }
+
+  @override
+  double derivative(double o) {
+    return 1.0;
+  }
+
+  @override
+  double derivativeWithFlatSpot(double o) {
+    return 1.0 + flatSpot;
+  }
+
+  @override
+  Float32x4 derivativeEntry(Float32x4 entry) {
+    return Float32x4(
+      1.0,
+      1.0,
+      1.0,
+      1.0,
+    );
+  }
+
+  @override
+  Float32x4 derivativeEntryWithFlatSpot(Float32x4 entry) {
+    return Float32x4(
+      1.0 + flatSpot,
+      1.0 + flatSpot,
+      1.0 + flatSpot,
+      1.0 + flatSpot,
+    );
+  }
+}
+
+class ActivationFunctionSigmoid extends ActivationFunction<double, Float32x4> {
+  const ActivationFunctionSigmoid({double initialWeightScale = 1})
+      : super(initialWeightScale);
+
+  static final List<ActivationFunctionScope> _scope = List.unmodifiable(
+      [ActivationFunctionScope.hidden, ActivationFunctionScope.output]);
+
+  @override
+  List<ActivationFunctionScope> get scope => _scope;
+
+  @override
+  double activate(double x) {
+    if (x < -700) {
+      return 0.0;
+    } else if (x > 700) {
+      return 1.0;
+    }
+
     return 1 / (1 + fast_math.exp(-x));
   }
 
   @override
   Float32x4 activateEntry(Float32x4 entry) {
     return Float32x4(
-      1 / (1 + fast_math.exp(-entry.x)),
-      1 / (1 + fast_math.exp(-entry.y)),
-      1 / (1 + fast_math.exp(-entry.z)),
-      1 / (1 + fast_math.exp(-entry.w)),
+      activate(entry.x),
+      activate(entry.y),
+      activate(entry.z),
+      activate(entry.w),
     );
   }
 
   @override
   double derivative(double o) {
     return o * (1.0 - o);
+  }
+
+  @override
+  double derivativeWithFlatSpot(double o) {
+    return o * (1.0 - o) + flatSpot;
   }
 
   @override
@@ -52,12 +148,25 @@ class ActivationFunctionSigmoid extends ActivationFunction<double, Float32x4> {
       entry.w * (1.0 - entry.w),
     );
   }
+
+  @override
+  Float32x4 derivativeEntryWithFlatSpot(Float32x4 entry) {
+    return Float32x4(
+      entry.x * (1.0 - entry.x) + flatSpot,
+      entry.y * (1.0 - entry.y) + flatSpot,
+      entry.z * (1.0 - entry.z) + flatSpot,
+      entry.w * (1.0 - entry.w) + flatSpot,
+    );
+  }
 }
 
 class ActivationFunctionSigmoidFast
     extends ActivationFunction<double, Float32x4> {
-  const ActivationFunctionSigmoidFast([int initialWeightScale = 2])
+  const ActivationFunctionSigmoidFast({double initialWeightScale = 1})
       : super(initialWeightScale);
+
+  @override
+  List<ActivationFunctionScope> get scope => ActivationFunctionSigmoid._scope;
 
   @override
   double activate(double x) {
@@ -100,10 +209,13 @@ class ActivationFunctionSigmoidBoundedFast
   final double scale;
 
   const ActivationFunctionSigmoidBoundedFast(
-      [this.scale = 6, int initialWeightScale = 2])
+      {this.scale = 6, double initialWeightScale = 2})
       : lowerLimit = -scale,
         upperLimit = scale,
         super(initialWeightScale);
+
+  @override
+  List<ActivationFunctionScope> get scope => ActivationFunctionSigmoid._scope;
 
   @override
   double activate(double x) {
@@ -144,8 +256,11 @@ class ActivationFunctionSigmoidBoundedFast
 
 class ActivationFunctionSigmoidFastInt100
     extends ActivationFunction<int, Int32x4> {
-  const ActivationFunctionSigmoidFastInt100([int initialWeightScale = 10])
+  const ActivationFunctionSigmoidFastInt100([double initialWeightScale = 10])
       : super(initialWeightScale);
+
+  @override
+  List<ActivationFunctionScope> get scope => ActivationFunctionSigmoid._scope;
 
   @override
   int activate(int x) {
@@ -184,9 +299,12 @@ class ActivationFunctionSigmoidFastInt
   final int scaleMax;
 
   const ActivationFunctionSigmoidFastInt(this.scaleMax,
-      [int initialWeightScale = 10])
+      [double initialWeightScale = 10])
       : scaleCenter = scaleMax ~/ 2,
         super(initialWeightScale);
+
+  @override
+  List<ActivationFunctionScope> get scope => ActivationFunctionSigmoid._scope;
 
   @override
   int activate(int x) {
